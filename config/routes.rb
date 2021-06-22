1dffc669d997eeb40703a50e33e7cd1b0a848691
mfrkw <xjdk3766@gmail.com>
2021-06-22 16:49:00 +0000

    [完成間際]

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :books,
-  resources :users
+  get 'users/show'
+  
+  resources :books, only: [:new, :create, :index, :show, :destroy ,:edit, :update]
+  resources :users, only: [:show, :edit, :update, :index]
 end
