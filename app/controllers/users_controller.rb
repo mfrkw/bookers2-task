7dc882e551fd293ba9a10cf3afdba351f852fad5
mfrkw <xjdk3766@gmail.com>
2021-06-22 10:24:06 +0000

    [あと少し]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users =User.all
+    @users = User.all
     @books = Book.all
     @book = Book.new
     @user = current_user
